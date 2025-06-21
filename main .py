7ce078e8b5bf4f5587a02a156b693df93469c1df
gunkaransingh22 <gunkaransingh22@gmail.com>
2025-06-21 10:32:11 +0530

    python operator 1

---
+++ main .py
@@ -0,0 +1,9 @@
+a = 5
+b = 6
+c = 7
+d = 8
+e = 9
+sum = a+b+c+d+e
+print(sum)
+avg = sum/5
+print(avg)
---
+++ main1.py
@@ -0,0 +1,9 @@
+math = int(input("enter the marks of math "))
+eng = int(input("enter the marks of eng "))
+sst = int(input("enter the marks of sst"))
+hindi = int(input("enter the marks of hindi"))
+gk = int(input("enter the marks of gk"))
+sum = math+eng+sst+hindi+gk
+print(sum)
+per = (sum/500)*100
+print(per)
